29f89a13aca5dffef7ef8807b7c92a23260fe585
Corvin Tiperciuc <cuvidk@gmail.com>
2024-04-02 22:21:36 +0300

    Hashes passwords before saving them

--- src/services/user/user-service.ts
+++ src/services/user/user-service.ts
@@ -1,17 +1,24 @@
 import { CreateUserIn, IUserService } from './types';
 import { IUserRepository, User } from '../../repository/user/types';
+import bcrypt from 'bcrypt';
 
 export class UserService implements IUserService {
   constructor(private _repo: IUserRepository) {}
 
   async createUser(params: CreateUserIn): Promise<User | undefined> {
-    return this._repo.addUser(params);
+    const hashedPassword = await bcrypt.hash(params.password, 10);
+    return this._repo.addUser({
+      ...params,
+      password: hashedPassword,
+    });
   }
 
   async validateUser(username: string, password: string): Promise<User | undefined> {
     const user = await this._repo.getUser<User>({ filter: { username } });
-    // use a comparison that's not vulnerable to timing attacks w/ bcrypt or smthing + store hashed instead
-    if (!user || user.password !== password) return;
+
+    if (!user) return;
+    if (!bcrypt.compareSync(password, user.password)) return;
+
     return user;
   }
 }
